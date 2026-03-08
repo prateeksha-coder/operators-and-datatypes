5b628c2d1f62381976739241ee1706fe690b06f8
prateeksha-coder <prateeksha.codingal@gmail.com>
2026-03-08 09:30:06 +0530

    initial commit

---
+++ a1.py
@@ -0,0 +1,16 @@
+name="Parteeksha"
+age=32
+num=16.07
+value=True
+print("Before Typecasting: ")
+print(name,type(name))
+print(age,type(age))
+print(num,type(num))
+print(value,type(value))
+print("After Typecasting: ")
+age1=float(age)
+print(age1,type(age1))
+num_1=int(num)
+print(num_1,type(num_1))
+value1=str(value)
+print(value1,type(value1))
